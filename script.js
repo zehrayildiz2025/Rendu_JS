b9bac8bc104d3dc49456609eaac8a105c7ede533
zehrayildiz2025 <zehra.yildiz@latoile.em-lyon.com>
2025-12-16 14:59:24 +0100

    innit 3

--- script.js
+++ script.js
@@ -32,26 +32,59 @@ fetch(jsonLink)
         // creer h2 et l'inserer dans la section
         let titleAv = document.createElement("h2");
         titleAv.textContent = "Avantages Clients";
-
+        // l'inserer dans la section
         avantageSection.appendChild(titleAv);
 
-        // // creer p et l'inserer dans la section
-        // let pAv = document.createElement("p");
-
-        // // creer carte d'avantages, faire un boucle forEach et l'inserer dans la section
+        // // creer carte d'avantages
 
         let cardsContainer = document.createElement("div");
         cardsContainer.classList.add("cardsContainerFlex");
 
-        
+        // faire un boucle forEach
         data.avantagesClients.forEach(elementAv => {
             let card = document.createElement("div");
             card.textContent = elementAv;
             cardsContainer.appendChild(card);
         });
+
+        // l'inserer dans la section
         avantageSection.appendChild(cardsContainer);
 
+        // Etape 4 : faire la section Service 
+        // cibler la section service
+        let serviceSection = document.getElementById("services");
+        // creer h2 et l'inserer dans la section
+        let titleServ = document.createElement("h2");
+        titleServ.textContent = "Services";
+        serviceSection.appendChild(titleServ);
 
+        // creer carte de services
+        let servicesContainer = document.createElement("div");
+        servicesContainer.classList.add("cardsContainerFlex");
 
+        // faire un boucle forEach
+        data.services.forEach(elementServ => {
+            let cardServ = document.createElement("div");
+
+            servicesContainer.appendChild(cardServ);
+            let titleCard = document.createElement("h3");
+            titleCard.textContent = elementServ.nom;
+            cardServ.appendChild(titleCard);
+
+            let descCard = document.createElement("p");
+            descCard.textContent = elementServ.description;
+            cardServ.appendChild(descCard);
+
+            let imageCard = document.createElement("img");
+            imageCard.classList.add("imgServ");
+            imageCard.src = elementServ["image-url"];
+            cardServ.appendChild(imageCard);
+
+        });
+        serviceSection.appendChild(servicesContainer);
+        // l'inserer dans la section
+
+        // Etape 5 : faire la section Temoignages
+        // creer carte d'avantages, faire un boucle forEach et l'inserer dans la section
 
     })
